1dfa8e011258bbadf8aec044ddcc63b34f02c98c
Diego Santos <diegoeustaquiodossantos@hotmail.com>
2019-09-22 13:40:09 -0300

    Projeto criado

---
+++ src/application/Program.java
@@ -0,0 +1,17 @@
+package application;
+
+/**
+ * @author Diego Santos
+ */
+public class Program {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hello Word!");
+        
+    }
+    
+}
