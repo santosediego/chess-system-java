2591ecc081bc0315c0c7c60f31533c2979722e6c
Diego Santos <diegoeustaquiodossantos@hotmail.com>
2020-01-16 06:49:02 -0300

    Moving pieces

--- src/chess/ChessException.java
+++ src/chess/ChessException.java
@@ -1,10 +1,12 @@
 package chess;
 
+import boardgame.BoardException;
+
 /**
  *
  * @author Diego Santos
  */
-public class ChessException extends RuntimeException{
+public class ChessException extends BoardException{
     public ChessException(String msg){
         super(msg);
     }
